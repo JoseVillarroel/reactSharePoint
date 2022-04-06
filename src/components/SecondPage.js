dfc98ea11bdfb978ba18163c3307efab81252261
robertoMendoza97 <robertoemendozac@gmail.com>
2022-04-06 18:33:55 -0400

    primer commmit cel proyecto

---
+++ src/components/SecondPage.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+// import { sp } from "";
+
+const SecondPage = () => {
+  const fetchData = async () => {
+    const data = await fetch(
+      `https://grupoentel.sharepoint.com/sites/Estacionamientos/Desarrollo/_api/web/lists/getbytitle('Usuario')/items`,
+      {
+        headers: { Accept: "application/json; odata=verbose;" }
+      }
+    );
+    const dataJson = await data.json();
+    console.log(dataJson);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      hola po whaso
+    </div>
+  );
+};
+
+// class secondPage extends React.Component {
+//   componentDidMount() {
+//     var reactHandler = this;
+//     jquery.ajax({
+//       url: `${this.props.siteurl}/_api/web/lists/getbytitle('EmployeeList')/items`,
+//       type: "GET",
+//       ,
+//       success: function (resultData) {
+//         reactHandler.setState({
+//           items: resultData.d.results
+//         });
+//       },
+//       error: function (jqXHR, textStatus, errorThrown) {}
+//     });
+//   }
+// }
+
+export default SecondPage;
